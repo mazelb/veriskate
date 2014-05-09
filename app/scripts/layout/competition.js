3070383296a10780798515c7a6c5771a6046a1c0
mazen elbawab <mazen@maz-e.com>
2014-05-09 00:42:44 -0400

    fixed competition links

--- app/scripts/layout/competition.js
+++ app/scripts/layout/competition.js
@@ -28,25 +28,33 @@
             }
         );
 
-        //holds the active status of the competition tabs
-        vm.competitionTabs = [
-            { active: true },
-            { active: false },
-            { active: false },
-            { active: false }
-        ];
+        ////holds the active status of the competition tabs
+        //vm.competitionTabs = [
+        //    { active: true },
+        //    { active: false },
+        //    { active: false },
+        //    { active: false }
+        //];
 
-        //Chose a different event 
-        vm.eventClicked = function (vIdx) {
-            for (var i = 0; i < vm.competitionTabs.length; i++) {
-                vm.competitionTabs[i].active = false;
-            }
-            vm.competitionTabs[vIdx].active = true;
-            datacontext.curEvent = vIdx;
-        }
+        ////Chose a different event 
+        //vm.eventClicked = function (vIdx) {
+        //    for (var i = 0; i < vm.competitionTabs.length; i++) {
+        //        vm.competitionTabs[i].active = false;
+        //    }
+        //    vm.competitionTabs[vIdx].active = true;
+        //    datacontext.curEvent = vIdx;
+        //}
 
-        vm.gotoLeaderBoard = function (vIdx) {
+        vm.gotoLeaderBoard = function (vComp, vIdx) {
             datacontext.curProgram = vIdx;
+
+            for (var i = 0; i < datacontext.CompetitionData.events.length; i++) {
+                if (datacontext.CompetitionData.events[i].event_name == vComp.event_name) {
+                    datacontext.curEvent = i;
+                    break;
+                }
+            }
+            
             $location.path('/leaderboard/');
             //window.location = '#/leaderboard/';
         }
